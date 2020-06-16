a2ddb082e9904dc65bf42bc4b8be9146ac1fbf4a
oscar0urselli <52245083+oscar0urselli@users.noreply.github.com>
2020-06-16 17:57:55 +0200

    Update sketch-ControlRGBLedStrip-Test1.ino

--- sketch-ControlRGBLedStrip-Test1.ino
+++ sketch-ControlRGBLedStrip-Test1.ino
@@ -1,16 +1,23 @@
 #include <FastLED.h>
 
-#define LED_PIN 6
-#define NUM_LEDS 150
+#define LED_PIN 6  // Pin of the signal
+#define NUM_LEDS 150  // Number of leds
 
 CRGB leds[NUM_LEDS];
 
-int colors[3];
+int colors[3];  // Colors of the led
+
+#define updateLEDS 1
 
 void setup() {
   Serial.begin(9600); //initialize serial COM at 9600 baudrate
   
-  FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
+  FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS);
+  
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds[1] = CRGB(0, 0, 0);
+  }
+  FastLED.show();
 }
 
 void loop() {
@@ -19,7 +26,12 @@ void loop() {
       colors[i] = Serial.read();
     }
 
-    leds[0] = CRGB(colors[0], colors[1], colors[2]);
+    for (int i = NUM_LEDS - 1; i >= updateLEDS; i--) {
+      leds[i] = leds[i - updateLEDS];
+    }
+    for (int i = 0; i < updateLEDS; i++) {
+      leds[i] = CRGB(colors[0], colors[1], colors[2]);
+    }
 
     FastLED.show();
   }
